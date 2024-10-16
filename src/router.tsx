b2867a1d48214bd5937badf47d636604e522edb6
tristanelain <tristan.elain@gmail.com>
2024-10-16 21:06:09 +0200

    fix basename

--- src/router.tsx
+++ src/router.tsx
@@ -2,13 +2,16 @@ import { createBrowserRouter } from "react-router-dom";
 import { Results } from "./page/results";
 import { Home } from "./page/root";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/results",
-    element: <Results />,
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/results",
+      element: <Results />,
+    },
+  ],
+  { basename: "/quizz-maker" }
+);
